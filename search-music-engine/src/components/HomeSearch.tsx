11aafac59a877dfb098d9c58ae29c76331bd26b1
alimukhtor <james1999007@gmail.com>
2022-02-02 14:49:27 +0100

    fetching music data

--- search-music-engine/src/components/HomeSearch.tsx
+++ search-music-engine/src/components/HomeSearch.tsx
@@ -1,8 +1,36 @@
-
+import {useState, useEffect} from 'react'
+import { IMusic } from './types/music'
 
 const HomeSearch =()=> {
+    const [searchInput, setSearchInput] = useState('')
+    const [music, setMusic] = useState<IMusic[]>([])
+    
+    const fetchMusic = async(searchInput="eminem")=> {
+        try {
+            const response = await fetch(`https://striveschool-api.herokuapp.com/api/deezer/search?q=${searchInput}`)
+            if(response.ok){
+                const data = await response.json()
+                setMusic(data.data)
+                // console.log(data);
+            }
+        } catch (error) {
+            console.log(error);
+            
+        }
+    }
+    useEffect(()=> {
+        fetchMusic()
+    }, [])
+
     return(
-        <h1>This is Home search</h1>
+        <>
+        {
+            music.map(song => (
+                <h1 key={song.id}>{song.title}</h1>
+
+            ))
+        }
+        </>
     )
 }
 export default HomeSearch
---
+++ search-music-engine/src/components/types/music.ts
@@ -0,0 +1,15 @@
+
+export interface IMusic{
+    id: number
+    title: String
+    cover: String
+    cover_Small:string
+    cover_Medium:string, 
+    cover_Big:string 
+    tracklist: String
+    name:string
+    link:string
+    picture:string
+    rank:number
+    duration:number
+}
